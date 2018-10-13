6bdb60baa90542dff885e2677a322ff335d7d2fa
Steven Xie <dev@stevenxie.me>
2018-10-13 11:16:42 -0400

    Configured deployment to Github Pages
    Also upgraded package dependencies.

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
-const isUWCS = process.env.UWCS === "true";
-if (isUWCS) console.log('Detected environment "UWCS".');
+let baseUrl = undefined;
 
-module.exports = {
-  baseUrl: isUWCS ? "/~ssxie/" : "/"
-};
+if (process.env.UWCS === "true") {
+  console.log("Detected environment: UWCS");
+  baseUrl = "/~ssxie/";
+} else if (process.env.GH_PAGES === "true") {
+  console.log("Detected environment: GH_PAGES");
+  baseUrl = "/tinytask/";
+}
+
+module.exports = { baseUrl };
